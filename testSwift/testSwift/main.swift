718e67906829d65005eedfdb7336c7b1913eb526
bobaldo <davidepatrizi@gmail.com>
2014-12-09 18:55:02 +0100

    add 1

---
+++ testSwift/testSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testSwift
+//
+//  Created by System Administrator on 12/9/14.
+//  Copyright (c) 2014 animation. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
