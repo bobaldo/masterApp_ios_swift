f93a78f8f48e6cecc700db118030602ef84e2ee9
bobaldo <davidepatrizi@gmail.com>
2014-12-13 12:53:28 +0100

    example

---
+++ MyPlayground2.playground/section-1.swift
@@ -0,0 +1,33 @@
+var miaTupla = (codice: 404, "errore")
+var (code, _) = miaTupla
+miaTupla.0
+miaTupla.codice
+code
+let miaStringa = "1"
+var numero = miaStringa.toInt()
+var mioOpt : String? = nil
+var a = 10
+-a
+--a
+a
+var e = 10
+-9 % 4
+8 % 2.5
+8/2.5
+var i=0
+++i
+i+=2
+
+
+var arr = ["a", "b"]
+arr+=["pippo"]
+arr.count
+arr.append("PD")
+arr.count
+arr[0...4] = ["b"]
+
+
+
+var arr[String]()
+arr.append("ppp")
+
--- testSwift/testSwift/main.swift
+++ testSwift/testSwift/main.swift
@@ -10,3 +10,21 @@ import Foundation
 
 println("Hello, World!")
 
+
+
+var thre = 3
+var digit = 0.1459
+
+var pi = Double(thre) + digit
+
+
+var a = "a"
+var b = "b"
+var c = a + b
+
+
+var miaTupla = (404, "Errore")
+var (primo, _)  = miaTupla
+
+
+
